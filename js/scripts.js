3f3ff2273b78f0b150ad9c529f74cf0475a388c8
serhankirtasiye <asf4807@gmail.com>
2025-01-20 18:08:43 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/*!
+* Start Bootstrap - Small Business v5.0.3 (https://startbootstrap.com/template/small-business)
+* Bootstrap'i Başlatın - Küçük İşletme v5.0.3 (https://startbootstrap.com/template/small-business)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+// Bu dosya kasıtlı olarak boş
+// Projenize JavaScript eklemek için bu dosyayı kullanın
